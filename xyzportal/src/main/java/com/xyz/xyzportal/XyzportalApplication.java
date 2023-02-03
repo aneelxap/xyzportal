f4ec39cdcbbb84d5f4b5882552fa96d89be10259
Anil Prajapati <105533596+aneelxap@users.noreply.github.com>
2023-02-03 16:31:03 +0530

    Add files via upload
    initials

---
+++ xyzportal/src/main/java/com/xyz/xyzportal/XyzportalApplication.java
@@ -0,0 +1,13 @@
+package com.xyz.xyzportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzportalApplication.class, args);
+	}
+
+}
